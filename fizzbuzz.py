d81174d0a8121ec957ffe36c99578d68eaf0e276
kaotu <kao-tu@hotmail.co.th>
2022-06-14 09:58:58 +0700

    Update fizzbuzz case 🌾
    Co-authored-by: Mild-TN <mild-tn@users.noreply.github.com>

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,30 +1,48 @@
 import unittest
 
 
-
-class FizzBuzz(unittest.TestCase):
+class FizzBuzzTest(unittest.TestCase):
     def test_input_3_return_Fizz(self):
         actual = fizz_buzz(3)
         expected = "Fizz"
-        self.assertEqual(actual, expected)
-        
+        self.assertEqual(expected, actual)
+
     def test_input_5_return_Buzz(self):
         actual = fizz_buzz(5)
         expected = "Buzz"
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
+    
+    def test_input_15_return_Fizz(self):
+        actual = fizz_buzz(15)
+        expected = "FizzBuzz"
+        self.assertEqual(expected, actual)
 
-    def test_input_2_return_Fizz(self):
-        actual = fizz_buzz(2)
-        expected = 2
-        self.assertEqual(actual, expected)
+    def test_input_10_return_Buzz(self):
+        actual = fizz_buzz(10)
+        expected = "Buzz"
+        self.assertEqual(expected, actual)
+    
+    def test_input_1_return_1(self):
+        actual = fizz_buzz(1)
+        expected = 1
+        self.assertEqual(expected, actual)
+        
+    def test_input_0_return_0(self):
+        actual = fizz_buzz(0)
+        expected = 0
+        self.assertEqual(expected, actual)
 
 
 def fizz_buzz(number):
-    if number == 5:
+    if number == 0:
+        return 0
+    if number % 5 == 0 and number % 3 == 0:
+        return "FizzBuzz"
+    if number % 5 == 0:
         return "Buzz"
-    if number == 2:
+    elif number % 3 == 0:
         return "Fizz"
-    
-    return "Fizz"
-    
+    return number
+
+
 unittest.main()
